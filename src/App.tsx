07bdb5a4d5d00d71b397f6ffd5b99fdb87cb34df
isabella <cisabella626@gmail.com>
2024-01-17 17:44:51 -0300

    adicionando bibliotecas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Home from './paginas/home/Home';
-import './App.css';
-
+import Navbar from './components/NavBar/Navbar';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/Login/Login';
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
-export default App;
+export default App; 
