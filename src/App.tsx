f69beaa0e7bc6452fe5df7c03dec7b682cae7a79
isabella <cisabella626@gmail.com>
2024-01-18 13:20:31 -0300

    juntando back-end com front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Home from './paginas/home/Home';
 import Navbar from './components/NavBar/Navbar';
 import Footer from './components/Footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/Login/Login';
+import Cadastro from './paginas/Cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <>
+     <AuthProvider>
     <BrowserRouter>
         <Navbar />
           <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
+          <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
 );
 }
