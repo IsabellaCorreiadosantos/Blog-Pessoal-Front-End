8d60028e11e178e340a7769ff869435aa90b6766
isabella <cisabella626@gmail.com>
2024-01-29 16:53:24 -0300

    adicionando cadastrar postagens editar postagens e deletar postagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 import Navbar from './components/NavBar/Navbar';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -11,6 +10,14 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/ListaTemas';
 import FormularioTema from './components/temas/FormularioTema';
 import DeletarTema from './components/temas/deletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+
+
+
+
+
 
 
 function App() {
@@ -29,6 +36,10 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,32 +1,35 @@
-import React,  { useContext } from 'react';
-import { UserContext } from '../../contexts/UserContext';
-import homeLogo from '../../assets/gamer.svg';
+import React from 'react';
+import homeLogo from '../../assets/gamer.svg'
 import './Home.css';
-import { Link, useNavigate } from 'react-router-dom';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
 
 
 function Home() {
-  const { nome, setNome } = useContext(UserContext);
     return (
         <>
-        <div className="bg-[#000000] flex justify-center">
+        <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem-vinde:</h2>
-              <h2 className='text-3xl font-bold' > {nome}</h2>
-              <Link to="/login" className='rounded bg-[#cc1111] text-[#000000] py-2 px-4'>
-          Voltar 
-        </Link>
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
+            </div>
+  
             <div className="flex justify-center ">
               <img src={homeLogo} alt="" className='w-2/3' />
       
             </div>
           </div>
         </div>
-      
+        <ListaPostagens />
       </>
     );
 }
 
-export default Home; 
+export default Home;
